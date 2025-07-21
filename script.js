35df1a97eb9e8b3136f74e27ef9c446b0a0c28a1
HG Chenxin <166237359+zanesirusen@users.noreply.github.com>
2025-07-21 09:42:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const toggleButton = document.getElementById("togglePlayPause");
 const audioList = [
   "assets/audios/audio1.mp3",
   "assets/audios/audio2.mp3",
-  "assets/audios/audio3.mp3"
+  "assets/audios/audio3.mp3",
+  "assets/audios/audio4.mp3"
 ];
 
 let audioIndex = 0;
